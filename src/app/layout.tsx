e46ed1544e36ece2e675707110eef1d0826fa3d8
Dorin Puscasu <46497424+dorinpuscasu25@users.noreply.github.com>
2026-07-20 11:30:20 +0300

    Update Google Ads tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,22 +27,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ro">
-      <Script id="google-tag-manager" strategy="beforeInteractive">
-        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5CD6S6M6');`}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=AW-18305702227"
+        strategy="afterInteractive"
+      />
+      <Script id="google-tag" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'AW-18305702227');`}
       </Script>
       <body className={montserrat.className}>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5CD6S6M6"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
         <Providers>{children}</Providers>
       </body>
     </html>
