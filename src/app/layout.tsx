bea872e5c9014d0c996be05b88cf36daf22ce74a
Dorin Puscasu <46497424+dorinpuscasu25@users.noreply.github.com>
2026-07-20 21:03:28 +0300

    Replace Google tag with GTM container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,17 +27,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ro">
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=AW-18305702227"
-        strategy="afterInteractive"
-      />
-      <Script id="google-tag" strategy="afterInteractive">
-        {`window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'AW-18305702227');`}
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5CD6S6M6');`}
       </Script>
       <body className={montserrat.className}>
         <Providers>{children}</Providers>
